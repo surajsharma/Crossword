3662bf56dc665a477931dd755cdb98aa528a8070
suraj <surajsharma@pm.me>
2020-02-05 01:48:22 +0530

    cell handles focus blur and change

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -4,12 +4,32 @@ import "../styles/cell.css";
 export default class Cell extends Component {
     constructor(props) {
         super(props);
-        this.state = { edting: false, value: props.value };
+        this.state = { editing: false, value: props.value, inputVal: "" };
     }
 
+    handleFocus = () => {
+        console.log("click");
+        this.setState({ editing: !this.state.editing, value: "" });
+    };
+
+    handleBlur = () => {
+        console.log("click");
+        this.setState({ editing: !this.state.editing });
+    };
+
+    handleChange = (e) => {
+        console.log(e.target.value);
+        this.setState({ value: e.target.value });
+    };
+
     render() {
-        const style =
-            this.props.value === "" ? "rgb(10, 10, 10)" : "rgb(200, 200, 200)";
+        const style = this.state.editing
+            ? "rgb(200,200,0)"
+            : this.state.value === ""
+            ? "rgb(10, 10, 10)"
+            : this.state.editing
+            ? "rgb(200,200,0)"
+            : "rgb(200, 200, 200)";
 
         const x =
             this.props.x === 1
@@ -32,20 +52,32 @@ export default class Cell extends Component {
                         style={{
                             fill: style,
                             strokeWidth: "0.4px",
-                            stroke: "black",
-                            margin: "5px"
+                            stroke: "black"
                         }}
                     />
                     <text x={x + 1} y={y + 2} className="small">
                         {this.props.number}
                     </text>
-                    <text x={x + 2} y={y + 8} className="heavy">
+                    <text
+                        x={x + 5}
+                        y={y + 5.5}
+                        className="heavy"
+                        dominantBaseline="middle"
+                        textAnchor="middle"
+                    >
                         {this.state.value}
                     </text>
                 </g>
                 <foreignObject x={x} y={y} width="9" height="9">
                     <div xmlns="http://www.w3.org/1999/xhtml">
-                        <input className="input"></input>
+                        <input
+                            maxLength="1"
+                            onFocus={this.handleFocus}
+                            onBlur={this.handleBlur}
+                            onChange={this.handleChange}
+                            className="input"
+                            value={this.state.inputVal}
+                        />
                     </div>
                 </foreignObject>
             </svg>
